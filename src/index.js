b7263e05cd08eed4112568a412498615d527a8ca
Ushieru <uzielcocolan@gmail.com>
2021-08-20 08:18:57 -0500

    todo-backend

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3300
+
+const todoList = [{
+    id: Date.now(),
+    task: 'Tarea Prueba'
+}]
+
+app.use(cors({
+    origin: '*'
+}))
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.send({ ok: 'ok' })
+})
+
+app.get('/todos', (request, response) => {
+    response.json({ todos: todoList })
+})
+
+app.post('/todo', (request, response) => {
+    const { task } = request.body;
+    if (!task) response.status(400).json({ error: 'No agregaste una tarea' });
+    todoList.push({
+        id: Date.now(),
+        task: task
+    })
+    response.json({ ok: 'ok' });
+})
+
+app.listen(port, () => {
+    console.log('[Server] App listening at localhost:' + port)
+})
